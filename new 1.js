a59710d9f22cb52557d3ebfb6cc5e35bc881a1b8
Darneti <Darneti@ukr.net>
2017-09-21 15:23:04 +0300

    Add files via upload

---
+++ new 1.js
@@ -0,0 +1,20 @@
+// Кнопка «Наверх/Вниз»
+var lastScrollPosition = 0; 
+
+$('#js-scroll-up').click( function(){
+	if ( $(document).scrollTop() > 0 ) {
+		$('body').animate({scrollTop:0},1000);
+		lastScrollPosition = $(document).scrollTop();
+	} else {
+		$('body').animate({scrollTop:lastScrollPosition},1000);
+	}	
+});
+
+$(document).scroll( function() {
+	if ( $(document).scrollTop() > 0 ) {
+		$('#js-scroll-up').fadeIn();
+		$('#js-scroll-up').text('Наверх');
+	} else {
+		$('#js-scroll-up').text('Вниз');
+	}
+});
