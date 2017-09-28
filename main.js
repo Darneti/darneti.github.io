949aa3337072bc20916791acddcb5d6e3ade267e
Darneti <Darneti@ukr.net>
2017-09-28 09:48:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
 // Кнопка «Наверх/Вниз»
-var lastScrollPosition = 0; 
-
-$('#js-scroll-up').click( function(){
-	if ( $(document).scrollTop() > 0 ) {
-		$('body').animate({scrollTop:0},1000);
-		lastScrollPosition = $(document).scrollTop();
-	} else {
-		$('body').animate({scrollTop:lastScrollPosition},1000);
-	}	
-});
-
-$(document).scroll( function() {
-	if ( $(document).scrollTop() > 500 ) {
-		$('#js-scroll-up').fadeIn();
-		$('#js-scroll-up').text('Наверх');
-	} else {
-		$('#js-scroll-up').text('Вниз');
-	}
+$(document).ready(function(){
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 0) {$('#js-scroll-up').fadeIn();
+				     } else {
+					     $('#js-scroll-up').fadeOut();
+				     }});
+$('#js-scroll-up').click(function () {
+	$('body,html').animate({scrollTop: 0}, 400); return false;});
 });
